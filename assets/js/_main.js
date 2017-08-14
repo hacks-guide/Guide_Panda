4b7aca68359a970b4325abbe5caa3bb2f833b53a
Plailect <Plailect@gmail.com>
2017-08-14 16:51:27 -0400

    update devGuide to be structured more like the standard Guide this will allow for translations aswell

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -29,8 +29,8 @@ $(document).ready(function(){
   $(".sticky").Stickyfill();
 
   var stickySideBar = function(){
-    var show = $(".author__urls-wrapper button").length === 0 ? $(window).width() > 1024 : !$(".author__urls-wrapper button").is(":visible");
-    // console.log("has button: " + $(".author__urls-wrapper button").length === 0);
+    var show = $(".author__urls-wrapper #toggle-nav").length === 0 ? $(window).width() > 1024 : !$(".author__urls-wrapper #toggle-nav").is(":visible");
+    // console.log("has button: " + $(".author__urls-wrapper #toggle-nav").length === 0);
     // console.log("Window Width: " + windowWidth);
     // console.log("show: " + show);
     //old code was if($(window).width() > 1024)
@@ -54,9 +54,9 @@ $(document).ready(function(){
 
   // Follow menu drop down
 
-  $(".author__urls-wrapper button").on("click", function() {
+  $(".author__urls-wrapper #toggle-nav").on("click", function() {
     $(".author__urls").fadeToggle("fast", function() {});
-    $(".author__urls-wrapper button").toggleClass("open");
+    $(".author__urls-wrapper #toggle-nav").toggleClass("open");
   });
 
   // init smooth scroll
@@ -96,5 +96,21 @@ $(document).ready(function(){
     closeOnContentClick: true,
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
+
+  if((window.location.href.indexOf("/he_IL/") > -1) || (window.location.href.indexOf("/ar_SA/") > -1)) {
+    $(".nav-selector").css("left", "3rem");
+    $(".lang-selector").css("left", "0");
+    $(".links-menu").css("right", "auto");
+    $(".lang-menu").css("right", "auto");
+    $(".links-menu").css("left", "3rem");
+    $(".lang-menu").css("left", "0");
+    $('.greedy-nav').prepend('<style>.hidden-links:before{right:auto !important;}</style>');
+    $('.greedy-nav').prepend('<style>.hidden-links:after{right:auto !important;}</style>');
+    $('.greedy-nav').prepend('<style>.hidden-links:before{left:14px !important;}</style>');
+    $('.greedy-nav').prepend('<style>.hidden-links:after{left:14px !important;}</style>');
+    $(".masthead__menu-item--lg").css("padding-right", "inherit");
+    $(".masthead__menu-item--lg").css("padding-left", "2em");
+    document.body.style.direction = "rtl";
+  }
 
 });
